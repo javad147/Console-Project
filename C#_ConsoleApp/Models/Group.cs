92227bac3fcaabac4578b80d8b17de136ec92070
javad147 <javadnb@code.edu.az>
2023-11-21 16:34:09 +0400

    create Group class

---
+++ C#_ConsoleApp/Models/Group.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__ConsoleApp.Models
+{
+    internal class Group : BaseEntity
+    {
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+    }
+}
