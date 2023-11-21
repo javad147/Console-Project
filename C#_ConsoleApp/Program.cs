a78cbb2e74f36f8d05d8a8786e06ca50e3fa06bf
javad147 <javadnb@code.edu.az>
2023-11-21 16:28:57 +0400

    create User class

--- C#_ConsoleApp/Program.cs
+++ C#_ConsoleApp/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿AppOperations app = new AppOperations();
+
+
+app.RegisterUser("Javad", "Bakirli", 25, "javad.bakirli@gmail.com", "password", "12345");
+
+
+app.LoginUser("javad.bakirli@gmail.com", "12345");
+
+
+app.ShowMainMenu();
